131636e8d958c31201fdf06948dd8d5469551bbe
Briac Ferte <briac.ferte@gmail.com>
2021-02-03 12:08:10 +0100

    add notes

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,11 +4,13 @@ module.exports = ({ env }) => ({
       default: {
         connector: 'mongoose',
         settings: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
+            // uri: env('DATABASE_URI),
+        host: env('DATABASE_HOST', 'strapi-cluster.rb5ft.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'backend'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'Ny0tbOa2Aqppv6rd'),
         },
         options: {},
       },
