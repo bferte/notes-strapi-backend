48de5bc0a78a3369e9fa4e83b959dc76fd8097d6
Briac Ferte <briac.ferte@gmail.com>
2021-02-03 11:10:55 +0100

    update database config

--- config/database.js
+++ config/database.js
@@ -4,16 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'strapi-cluster.rb5ft.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'backend'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'Ny0tbOa2Aqppv6rd'),
+        uri: env('DATABASE_URI')
+        // host: env('DATABASE_HOST', 'strapi-cluster.rb5ft.mongodb.net'),
+        // srv: env.bool('DATABASE_SRV', true),
+        // port: env.int('DATABASE_PORT', 27017),
+        // database: env('DATABASE_NAME', 'backend'),
+        // username: env('DATABASE_USERNAME', 'strapi'),
+        // password: env('DATABASE_PASSWORD', 'Ny0tbOa2Aqppv6rd'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        //authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: true //env.bool('DATABASE_SSL', true),
       },
     },
   },
