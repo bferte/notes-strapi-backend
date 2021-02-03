b25be7cc6eb15a4706a9f7a33eef7e6de18ca802
Briac Ferte <briac.ferte@gmail.com>
2021-02-03 10:36:37 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce1ce417445e3a5f0d262dfe97909104'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f06441b2-e5c7-4b54-8eb9-b59bc69f8fed'
+};
